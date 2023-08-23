d4b6d97c6c89ed284757d75f1a29927b2c3a6a2e
sandman627 <hart627@g.skku.edu>
2023-08-23 12:24:34 +0900

    not sure of task config

--- env_var.sh
+++ env_var.sh
@@ -2,3 +2,7 @@
 
 export DISPLAY=:0.0
 export ALFWORLD_DATA=/home/sandman/check_envs/alfworld_data_storage/
+
+
+# metaworld environment variable
+export MUJOCO_PY_MUJOCO_PATH=/home/sandman/.mujoco/mujoco210
